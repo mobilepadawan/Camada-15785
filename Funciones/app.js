3ac45391572a3e7e01b89433995da57971dd182c
Fernando Luna <fernando@vidamobile.com.ar>
2021-03-02 19:18:20 -0300

    Initial commit

---
+++ Funciones/app.js
@@ -0,0 +1,45 @@
+// let num1 = prompt("Ingresa un número")
+// let num2 = prompt("Ingresa otro número")
+//    calcular()
+
+//FUNCIONES COMUNES
+// function calcular() { //que primero arroje concatenación
+//     console.log(num1 + num2)
+// }
+
+//FUNCIONES COMUNES COMBINADAS CON MÉTODOS PROPIOS DEL LENGUAJE
+// function calcular() {
+//     console.log(parseInt(num1) + parseInt(num2))
+// }
+
+//FUNCIONES CON PARÁMETROS
+// function calcular(num1, num2) {
+//     console.log(parseInt(num1) + parseInt(num2))
+// }
+
+//FUNCIONES CON PARÁMETROS Y RETORNO
+// function calcular(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log(calcular(parseInt(num1), parseInt(num2)))
+
+//EJEMPLO DIAPOSITIVA 59
+
+// let resultado = 0
+
+// function sumar(pn, sn) {
+//     resultado = parseInt(pn) + parseInt(sn)
+// }
+
+// function mostrar(mensaje) {
+//     console.log(mensaje)
+// }
+
+// sumar(6, 3)
+// mostrar(resultado)
+
+//DATA ADICIONAL
+
+//parseInt()
+//parseFloat()
