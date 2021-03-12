3ac1222b7461d43c8955181645ec861b1079c75b
Fernando O <fernando@vidamobile.com.ar>
2021-03-12 18:54:05 -0300

    Another new update

--- After Class 06 - Arrays/js/arrays.js
+++ After Class 06 - Arrays/js/arrays.js
@@ -1,38 +1,31 @@
 // //OPERACIONES SOBRE ARRAYS
-const paises = ["Argentina", "Uruguay", "Chile", "Brasil", "Perú", "Bolivia", "Paraguay", "Colombia", "Venezuela", "Ecuador"] //para usar con concat()
+const paises = ["Argentina", "Uruguay", "Chile", "Brasil", "Perú", "Bolivia", "Paraguay", "Colombia", "Venezuela", "Ecuador"]
 
 //["A", "Ante", "Bajo", "Cabe", "Con", "Contra", "De", "Desde", "En", "Entre", "Hacia", "Hasta", "Para", "Por", "Según", "Sin", "So", "Sobre", "Tras"]
 
 let elementos = []
 
-function cargoNuevoElemento() {
-    elementos.push(prompt("Ingrese un nuevo elemento: "))
-}
 
-function listarElementos() {
-    document.getElementById("listaElementos").innerHTML = ""
-    for (let e of elementos) {
-        document.getElementById("listaElementos").innerHTML += `<li>${e}</li>`       //"<li>" + e + "</li>"
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //ARRAY DE OBJETOS
-function altaDeNuevoProducto() {
-    const nuevoProducto = {productoid: 0, nombre: "", stock: 0, precio: 0}
-            nuevoProducto.productoid = Number(misProductos.length + 1)
-            nuevoProducto.nombre = prompt("Ingrese el nombre del producto nuevo: ")
-            nuevoProducto.stock = parseInt(prompt("Ingrese el stock inicial: "))
-            nuevoProducto.precio = parseFloat(prompt("Ingrese el precio de venta: "))
-            misProductos.push(nuevoProducto)
-}
-
-
-
-
-
-
-
-
 let misProductos = [{productoid: 1, nombre: "Macbook Air", stock: 30, precio: 950},
                     {productoid: 2, nombre: "EXO i3", stock: 10, precio: 1105},
                     {productoid: 3, nombre: "Lenovo i7", stock: 22, precio: 1200},
@@ -42,14 +35,3 @@ let misProductos = [{productoid: 1, nombre: "Macbook Air", stock: 30, precio: 95
                     //nuevoProducto = {productoid: (misProductos.length + 1), nombre: "iMac 21", stock: 35, precio: 1075}
                     //nuevoProducto = {productoid: (misProductos.length + 1), nombre: "iMac 27", stock: 18, precio: 1300}
                     //misProductos.push(nuevoProducto) //agrega el nuevo producto al array misProductos
-
-
-//ejemplo find(p => p.nombre.includes("iPad"))
-
-//ejemplo filter(p => p.nombre.includes("iMac"))
-
-// ejemplo filter(p => p.precio < 1000)
-
-//ejemplo map(p => p.nombre.stock = Number(p.nombre.stock * 1.15).toFixed(2))
-
-//SI NO QUIEREN PERDER LO CARGADO localStorage.misProductos = JSON.stringify(misProductos)
