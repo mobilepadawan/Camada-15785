b267ae1f1e33a6ffb572618c784883197a4847fb
Fernando Luna <fernando@vidamobile.com.ar>
2021-03-12 17:42:48 -0300

    Another update

--- After Class 06 - Arrays/js/arrays.js
+++ After Class 06 - Arrays/js/arrays.js
@@ -21,3 +21,36 @@ let misProductos = [{productoid: 1, nombre: "Macbook Air", stock: 30, precio: 95
                     //nuevoProducto = {productoid: (misProductos.length + 1), nombre: "iMac 21", stock: 35, precio: 1075}
                     //nuevoProducto = {productoid: (misProductos.length + 1), nombre: "iMac 27", stock: 18, precio: 1300}
                     //misProductos.push(nuevoProducto) //agrega el nuevo producto al array misProductos
+
+//["A", "Ante", "Bajo", "Cabe", "Con", "Contra", "De", "Desde", "En", "Entre", "Hacia", "Hasta", "Para", "Por", "Según", "Sin", "So", "Sobre", "Tras"]
+
+let elementos = []
+
+function cargoNuevoElemento() {
+    elementos.push(prompt("Ingrese un nuevo elemento"))
+}
+
+function listarElementos() {
+    for (let e of elementos) {
+        document.getElementById("listaElementos").innerHTML += `<li>${e}</li>`       //"<li>" + e + "</li>"
+    }
+}
+
+
+
+//ARRAY DE OBJETOS
+function altaDeNuevoProducto() {
+    const nuevoProducto = {productoid: 0, nombre: "", stock: 0, precio: 0}
+            nuevoProducto.productoid = Number(misProductos.length + 1)
+            nuevoProducto.nombre = prompt("Ingrese el nombre del producto nuevo: ")
+            nuevoProducto.stock = parseInt(prompt("Ingrese el stock inicial: "))
+            nuevoProducto.precio = parseFloat(prompt("Ingrese el precio de venta: "))
+            misProductos.push(nuevoProducto)
+}
+
+
+//ejemplo find(p => p.nombre.includes("iPad"))
+
+//ejemplo filter(p => p.nombre.includes("iMac"))
+
+//ejemplo map(p => p.nombre.stock = (p.nombre.stock * 1.15).toFixed(2))
