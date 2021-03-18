f3d980a9271026ff5c1f7f56231f040d7eca4687
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2021-03-18 16:33:26 -0300

    Base exercise for the next After Class (08)

---
+++ AC08 - DOM - Base/js/arrays.js
@@ -0,0 +1,18 @@
+let CURSOS = [
+    {
+    "nombre": "JAVASCRIPT",
+    "precio": 15000
+    },
+    {
+    "nombre": "PYTHON",
+    "precio": 12500
+    },
+    {
+    "nombre": "DESARROLLO WEB",
+    "precio": 14000
+    },
+    {
+    "nombre": "PROGRAMACON BACKEND",
+    "precio": 21500
+    }
+]    
