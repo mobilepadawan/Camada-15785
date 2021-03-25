f30f2f1370ccb9067e12d5e64ca191582118cd4a
Fernando Luna <fernando@vidamobile.com.ar>
2021-03-24 22:36:42 -0300

    more changes

--- Workshop Cotizador/js/parametros.js
+++ Workshop Cotizador/js/parametros.js
@@ -5,12 +5,12 @@ const arrayResidencia = ["CABA", "La Plata", "Tandil", "Costa Atlántica"]
 //Si agregamos nuevos tipos de vivienda, debemos definir el factor correspondiente a éstas, en el JSON homónimo
 const arrayTipoVivienda = ["Casa", "P.H.", "Edificio Departamento", "Barrio Privado"]
 
-let jsonAREA = [{"area": "CABA", "factor": 1.02}, 
+const jsonAREA = [{"area": "CABA", "factor": 1.02}, 
                 {"area": "La Plata", "factor": 1.03},
                 {"area": "Tandil", "factor": 0.92}, 
                 {"area": "Costa Atlántica", "factor": 1.15}]
 
-let jsonTIPOVIVIENDA = [{"tipo": "Casa", "factor": 1.05},
+const jsonTIPOVIVIENDA = [{"tipo": "Casa", "factor": 1.05},
                         {"tipo": "P.H.", "factor": 1.01},
                         {"tipo": "Edificio Departamento", "factor": 1.00},
                         {"tipo": "Barrio Privado", "factor": 1.12}]
