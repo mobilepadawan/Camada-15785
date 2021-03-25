a9e56736fd1ec26352f406a9c7e4afefbe480020
Fernando Luna <fernando@vidamobile.com.ar>
2021-03-25 19:46:55 -0300

    Base exercise

---
+++ Workshop Cotizador - Base/js/parametros.js
@@ -0,0 +1,11 @@
+const costoSeguroM2 = 28.75
+
+const jsonAREA = [{"area": "CABA", "factor": 1.02}, 
+                  {"area": "La Plata", "factor": 1.03},
+                  {"area": "Tandil", "factor": 0.92}, 
+                  {"area": "Costa Atlántica", "factor": 1.15}]
+
+const jsonTIPOVIVIENDA = [{"tipo": "Casa", "factor": 1.05},
+                          {"tipo": "P.H.", "factor": 1.01},
+                          {"tipo": "Edificio Departamento", "factor": 1.00},
+                          {"tipo": "Barrio Privado", "factor": 1.12}]
---
+++ Workshop Cotizador - Base/js/variables.js
@@ -0,0 +1,6 @@
+let objCotizar = ""                                                 //Variable que usaremos con el OBJETO COTIZADOR
+const areaResidencia = document.getElementById("areaResidencia")    //Combo Select RESIDENCIA
+const tipoVivienda = document.getElementById("tipoVivienda")        //Combo Select TIPO DE VIVIENDA
+const metros2 = document.getElementById("metros2")                  //Input METROS CUADRADOS DE LA PROPIEDAD
+const importeCuota = document.getElementById("importeCuota")        //Label QUE MUESTRA EL VALOR DE LA CUOTA
+const btnCotizar = document.getElementById("btnCotizar")            //BOTON QUE DISPARA LA COTIZACON
