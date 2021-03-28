a41a30e7578a948c872600485bd0bdefddade825
Fernando O <fernando@vidamobile.com.ar>
2021-03-27 21:46:59 -0300

    Final class changes

--- Jquery Eventos - Base/js/mijquery.js
+++ Jquery Eventos - Base/js/mijquery.js
@@ -1,42 +1,37 @@
-const CURSOS = [{id: 1, producto: "CURSO JAVASCRIPT", importe: "$ 15000"},
-                {id: 2, producto: "CURSO REACT", importe: "$ 16000"},
-                {id: 3, producto: "CURSO NODE (backend)", importe: "$ 18500"}]
+const PLANES = [{planid: 1, cardID: "planBronze", nombre: "Plan Bronze", infoA: "Landing page", infoB: "Sitio de una sección", infoC: "Dominio gratuito", precio: 4500},
+               {planid: 2, cardID: "planGold", nombre: "Plan Gold", infoA: "Landing page", infoB: "Sitio de hasta diez secciones", infoC: "Dominio gratuito",precio: 12500},
+               {planid: 3, cardID: "planSilver", nombre: "Plan Silver", infoA: "Landing page", infoB: "Sitio de hasta tres secciones", infoC: "Dominio gratuito", precio: 8500}]
 
+function cargoCards() {
+   cardsPlanes.innerHTML = ""
+   for (let plan of PLANES) {
+       let card = `<div class="col-lg-4 box">
+                       <h3>${plan.nombre}</h3>
+                       <h4>$ ${plan.precio}<span>[única vez]</span></h4>
+                       <ul>
+                         <li class=" pricing__box--list"><i class="bx bx-check"></i>${plan.infoA}</li>
+                         <li class=" pricing__box--list"><i class="bx bx-check"></i>${plan.infoB}</li>
+                         <li class=" pricing__box--list"><i class="bx bx-check"></i>${plan.infoC}</li>
+                       </ul>
+                       <button class="get-started-btn" onclick="guardoPlan(${plan.planid})">¡LO QUIERO!</a>
+                   </div>`
+                   cardsPlanes.innerHTML += card
+   }
+}
 
-$("#btnAgregar").click(()=> {
-   let curso = {id: 4, producto: "CURSO PYTHON", importe: "$ 12000"}
-      
+//CREO BOTON PARA INTERACTUAR Y GUARDAR DATOS EN STORAGE
+function guardoPlan(PLANES) {
+   localStorage.clear()
+   let planElegido = {nombre: "", precio: 0, ID: 0}
+   planElegido.nombre = PLANES.nombre
+   planElegido.precio = PLANES.precio
+   planElegido.ID = PLANES.planid
+   contein = JSON.stringify(planElegido)
+   localStorage.setItem("planElegido", contein)
+   console.log(planElegido)
+}
 
-      curso.id = $(":input#id").val()
-      curso.producto = $(":input#producto").val()
-      curso.importe = $(":input#importe").val()
-      CURSOS.push(curso)
-})
+const cardsPlanes = document.getElementById("cardsPlanes")
+//const btnLoQuiero = document.getElementsByTagName("button")
 
-
-$(document).ready(function() {
-   console.log("El DOM se ha cargado exitosamente.")
-
-   let divCotizador = $("#cotizador")
-       divCotizador.append('<input type="number" id="monto" placeholder="Monto a comprar">')
-       divCotizador.append('<input type="number" id="cotizacion" placeholder="Ingrese cotización del día">')
-       divCotizador.append('<br><br>')
-       divCotizador.append('<div class="center">')
-       divCotizador.append('<button id="btnCotizar" class="btn green darken-2 white-text waves-effect waves-light">COMPRAR</button>')
-       divCotizador.append('</div>')
-
-      $("#btnCotizar").click(()=> {
-         let monto = $("#monto").val()
-         let cotizacion = $("#cotizacion").val()
-            alert(monto * cotizacion)
-      })
-
-      $(":input").change((e)=> {
-         console.log(`El campo ${e.target.id} tiene el valor ${e.target.value}`)
-      })
-
-      $("form").submit(function(e) {
-         e.preventDefault()
-         console.log($(":input"))
-      })
-})
+document.addEventListener("DOMContentLoaded", cargoCards)
