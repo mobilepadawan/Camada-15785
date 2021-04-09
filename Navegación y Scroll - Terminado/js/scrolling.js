0a2395af723dcf4daa9375186afce0544f41e347
Fernando O <fernando@vidamobile.com.ar>
2021-04-09 15:32:04 -0300

    Adding a second exercise to the first Class

---
+++ Navegación y Scroll - Terminado/js/scrolling.js
@@ -0,0 +1,13 @@
+function desplazarMe(titulo) {
+   $('html, body').animate({
+      scrollTop: $('#' + titulo).offset().top
+   }, 1000)
+}
+
+$('#link2').click(function() {
+   desplazarMe("titulo2")
+})
+
+$('#link3').click(function() {
+   desplazarMe("titulo3")
+})
