4f80972d7001337933bf1861e6f26a15e1912155
Fernando O <fernando@vidamobile.com.ar>
2021-04-09 15:59:16 -0300

    Final improvements

--- Navegación y Scroll - Terminado/js/scrolling.js
+++ Navegación y Scroll - Terminado/js/scrolling.js
@@ -1,3 +1,14 @@
+const desplazarMe = (titulo) => {
+   $('html, body').animate({
+      scrollTop: $(`#${titulo}`).offset().top
+   }, 2000)
+}
+
+$('#link1').click(()=> {desplazarMe("titulo1")})
+$('#link2').click(()=> {desplazarMe("titulo2")})
+$('#link3').click(()=> {desplazarMe("titulo3")})
+
+/* //Agregamos el control de tiempo para suavizar el desplazamiento
 const desplazarMe = (titulo, vel) => {
    $('html, body').animate({
       scrollTop: $(`#${titulo}`).offset().top
@@ -6,4 +17,4 @@ const desplazarMe = (titulo, vel) => {
 
 $('#link1').click(()=> {desplazarMe("titulo1", 1000)})
 $('#link2').click(()=> {desplazarMe("titulo2", 2000)})
-$('#link3').click(()=> {desplazarMe("titulo3", 3000)})
+$('#link3').click(()=> {desplazarMe("titulo3", 3000)}) */
