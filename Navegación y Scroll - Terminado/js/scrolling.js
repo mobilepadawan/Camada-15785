4f2aa8aaac7a985ee98eeee6c795d77b312857a5
Fernando O <fernando@vidamobile.com.ar>
2021-04-09 16:13:27 -0300

    Base coding for Students

--- Navegación y Scroll - Terminado/js/scrolling.js
+++ Navegación y Scroll - Terminado/js/scrolling.js
@@ -8,7 +8,8 @@ $('#link1').click(()=> {desplazarMe("titulo1")})
 $('#link2').click(()=> {desplazarMe("titulo2")})
 $('#link3').click(()=> {desplazarMe("titulo3")})
 
-/* //Agregamos el control de tiempo para suavizar el desplazamiento
+/*
+//Agregamos el control de tiempo para suavizar el desplazamiento
 const desplazarMe = (titulo, vel) => {
    $('html, body').animate({
       scrollTop: $(`#${titulo}`).offset().top
@@ -17,4 +18,5 @@ const desplazarMe = (titulo, vel) => {
 
 $('#link1').click(()=> {desplazarMe("titulo1", 1000)})
 $('#link2').click(()=> {desplazarMe("titulo2", 2000)})
-$('#link3').click(()=> {desplazarMe("titulo3", 3000)}) */
+$('#link3').click(()=> {desplazarMe("titulo3", 3000)})
+*/
