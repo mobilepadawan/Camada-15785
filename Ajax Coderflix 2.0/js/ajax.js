c38876351a16a4b26473b59d933e1cb66b18cf4c
Fernando O <fernando@vidamobile.com.ar>
2021-04-09 20:57:51 -0300

    Update ajax.js

--- Ajax Coderflix 2.0/js/ajax.js
+++ Ajax Coderflix 2.0/js/ajax.js
@@ -1,7 +1,6 @@
 $(document).ready(()=> {
     $("#titulo").show("slow", ()=> {
-        
-        $.getJSON("http://localhost:5501/js/coderflix.json", (response, status)=> {
+        $.getJSON("js/coderflix.json", (response, status)=> {
             if (status === "success") {
                 let contenido = response
                     for (const film of contenido) {
@@ -28,18 +27,18 @@ const cargoCard = (film) => {
             if (film.temporadas == "N/A")
                 leyenda = "DURACIÓN"
             movieOrSerie = `<p class="yellow-text">${leyenda}: <span class="white-text">${film.temporadas}</span></p>`
-            let card = `<div class="col s12 m6 l4">
-                            <div class="card z-depth-2">
-                                <div class="card-image">
-                                <img src="${film.poster}">
-                                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">search</i></a>
-                                </div>
-                                <div class="card-content black">
-                                <p class="yellow-text">GÉNERO: <span class="white-text">${film.genero}</span></p>
+            const card = `<div class="col s12 m6 l4">
+                             <div class="card z-depth-2">
+                                 <div class="card-image">
+                                 <img src="${film.poster}">
+                                 <a class="btn-floating halfway-fab waves-effect waves-light red"><i  class="material-icons">search</i></a>
+                                 </div>
+                                 <div class="card-content black">
+                                 <p class="yellow-text">GÉNERO: <span class="white-text">${film.genero}</span></p>
                                 ${movieOrSerie}
-                                </div>
-                            </div>
-                        </div>`
+                                 </div>
+                             </div>
+                         </div>`
                 return card
     }
 }
