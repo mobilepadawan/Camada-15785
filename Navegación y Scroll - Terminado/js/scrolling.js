2831b637b8674acb158b4aed8b3461bd8eb3c79b
Fernando O <fernando@vidamobile.com.ar>
2021-04-09 15:49:33 -0300

    Converting to arrow functions

--- Navegación y Scroll - Terminado/js/scrolling.js
+++ Navegación y Scroll - Terminado/js/scrolling.js
@@ -1,9 +1,8 @@
-function desplazarMe(titulo) {
+const desplazarMe = (titulo) => {
    $('html, body').animate({
-      scrollTop: $('#' + titulo).offset().top
+      scrollTop: $(`#${titulo}`).offset().top
    }, 1000)
 }
 
-$('#link2').click(function() {desplazarMe("titulo2")})
-
-$('#link3').click(function() {desplazarMe("titulo3")})
+$('#link2').click(()=> {desplazarMe("titulo2")})
+$('#link3').click(()=> {desplazarMe("titulo3")})
