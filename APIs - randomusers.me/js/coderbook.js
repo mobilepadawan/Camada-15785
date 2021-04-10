d9a1c1ba11801ac9a124c01e0727ae2d9b4d15cf
Fernando O <fernando@vidamobile.com.ar>
2021-04-09 23:39:39 -0300

    Coderbook adjustments

---
+++ APIs - randomusers.me/js/coderbook.js
@@ -0,0 +1,44 @@
+let HTMLTabla = "<h4>Resultados</h4>"
+let HTMLCard = ""
+let contenidoJSON = ""
+
+function cargoContenidoStreaming() {
+   $.ajax({
+      url: 'https://randomuser.me/api/?results=70&nat=es&gender=female',
+      dataType: 'json',
+      success: function(data) {
+         contenidoJSON = data.results
+         console.log(data)
+         for (let user of contenidoJSON) {
+            HTMLTabla += `<tr class="white">
+                              <td><img src="${user.picture.medium}" class="circle responsive-img"></td>
+                              <td>${user.name.first} ${user.name.last}</td>
+                              <td>${user.location.city}, ${user.location.country}</td>
+                              <td class="blue-text">${user.email}</td>
+                          </tr>`
+            $("#tabladeresultado").html(HTMLTabla)
+         };
+      },
+      error: function() {
+         HTMLCard = `<div class="center black-text ancho">
+                        <br><br>
+                        <br><br>
+                        <h4>El contenido parece no estar disponible. Intente nuevamente en unos minutos.</h4>
+                        <br><br>
+                        <i class="medium material-icons">sentiment_very_dissatisfied</i>
+                        <br><br>
+                     </div>`
+         $("#tabladeresultado").html(HTMLCard)
+      }
+   })
+}
+
+setTimeout(() => {
+   cargoContenidoStreaming()
+      $('#contenido').fadeIn("fast", function() {
+         $('#cargando').fadeOut(1000)
+      })
+}, 100)
+
+/* https://randomuser.me/
+https://randomuser.me/documentation#howto */
