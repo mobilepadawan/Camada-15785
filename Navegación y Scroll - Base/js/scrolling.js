a2f4a9f0f44276a2430d3c77afd9b1ab2879fd3f
Fernando O <fernando@vidamobile.com.ar>
2021-04-12 19:33:40 -0300

    SPA Jquery

--- Navegación y Scroll - Base/js/scrolling.js
+++ Navegación y Scroll - Base/js/scrolling.js
@@ -1,17 +1,9 @@
-const desplazarMe = (titulo)=> {
-   $('html', 'body').animate({
+const desplazarMe = (titulo, vel) => {
+   $('html, body').animate({
       scrollTop: $(`#${titulo}`).offset().top
-   }, 2000)
+   }, vel)
 }
 
-$("#link1").click(()=> {
-   desplazarMe("titulo1")
-})
-
-$("#link2").click(()=> {
-   desplazarMe("titulo2")
-})
-
-$("#link3").click(()=> {
-   desplazarMe("titulo3")
-})
+$('#link1').click(()=> {desplazarMe("titulo1", 1000)})
+$('#link2').click(()=> {desplazarMe("titulo2", 2000)})
+$('#link3').click(()=> {desplazarMe("titulo3", 3000)})
---
+++ SPA JQuery - Terminado/js/animaciones.js
@@ -0,0 +1,77 @@
+let productos = []
+const tableBody = $("tbody")
+const productoDesc = $("#descripcion")
+const cantidadPlazas = $("#cantidad")
+
+$("h1").css({"font-size": "30px", "color": "yellow", "text-decoration": "underline"})
+
+// "[{"pid":1,"producto":"Curso JavaScript","cantidad":90},{"pid":2,"producto":"Curso React","cantidad":100},{"pid":3,"producto":"Curso Node JS","cantidad":120},{"pid":4,"producto":"Curso PHP Backend","cantidad":90},{"pid":5,"producto":"Curso de SQL Server","cantidad":50}]"
+
+const creoFilaProducto =(producto) => {
+    const fid = (`fila${producto.pid}`).trim() //Creo un atributo ID dinámico para <tr>
+    const fila = `<tr id="${fid} style="display: none;">
+                     <td>${producto.pid}</td>
+                     <td>${producto.producto}</td>
+                     <td class="right">${producto.cantidad}</td>
+                     <td class="center"><button onclick="eliminarFilaYproducto('${fid}', ${producto.pid})" class="btn-small btn-floating red waves-effect waves-light"><i class="material-icons">delete</i></button></td>
+                 </tr>`
+    tableBody.append(fila)
+    $(`#${fid}`).show(300)
+}
+
+const generoID = ()=> (productos.length + 1)
+
+const agregarProducto =()=> {
+    if (productoDesc.val().trim() != "" && Number(cantidadPlazas.val()) > 0) {
+        const nuevoProducto = {
+                                pid: generoID(), 
+                                producto: productoDesc.val(),
+                                cantidad: Number(cantidadPlazas.val())
+                              }
+        productos.push(nuevoProducto)
+        guardarProductosEnLS()
+        $(":input").val("") //Aprovecho el selector :input, para vaciar todos los campos de una sola vez
+        creoFilaProducto(nuevoProducto)
+        console.info(`Se agregó el producto '${nuevoProducto.producto}', exitosamente.`)
+    } else {
+        console.error("No hay contenido para agregar en 'productos' :/ ")
+    }
+}
+
+const guardarProductosEnLS = ()=> {
+    if (productos.length > 0)
+    localStorage.cursos = JSON.stringify(productos)
+}
+
+const recuperarProductosDeLS = ()=> {
+    if (localStorage.cursos != undefined)
+        productos = JSON.parse(localStorage.cursos)
+}
+
+const eliminarFilaYproducto = (fid, pid) => {
+    productos.splice(pid, 1)
+    $(`#${fid}`).hide(2000, ()=> {
+        $(`#${fid}`).remove()
+        console.warn(`${fid} fue eliminado.`)
+    })
+}
+
+const eliminarProducto =(pid)=> {
+    productos.splice(pid)
+}
+
+const actualizoTablaProductos = () => {
+    if (productos.length > 0) {
+        tableBody.html("")
+        for (let p of productos) {
+            creoFilaProducto(p)
+        }
+    }
+}
+
+$("#guardar").click(()=> {agregarProducto()})
+
+$(()=> {
+    recuperarProductosDeLS()
+    actualizoTablaProductos()
+})
