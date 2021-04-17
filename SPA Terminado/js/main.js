8665a866fe1aa4ec49fc692d056b313ada0bc231
Fernando O <fernando@vidamobile.com.ar>
2021-04-17 18:28:23 -0300

    Final adjustment for the SPA projects

--- SPA Terminado/js/main.js
+++ SPA Terminado/js/main.js
@@ -1,65 +1,61 @@
-// PASO 1 - VERIFICAMOS EL ARCHIVO PRODUCTOS.JS e INDEX.HTML
-
-
-// PASO 2 - INSTANCIAMOS CONTROLADOR DE PRODUCTOS
+//PASO 2 - instanciar controlador de productos, integrando las clases Modelo y Vista
 const app = new ProductoController(new ProductoModel(), new ProductoView())
 
-const routes = [
-  {path: '', action: 'agregar' },
-  {path: '/', action: 'agregar' },
-  {path: '/pagina1', action: 'listar' },
-  {path: '/pagina2', action: 'buscar' }
-]
+const routes = [{path: '', action: 'agregar'},
+                {path: '/', action: 'agregar'},
+                {path: '/pagina1', action: 'listar'},
+                {path: '/pagina2', action: 'buscar'}]
 
-// PASO 3 - ARMAMOS EL COMPONENTE ERROR PARA MOSTRAR CUANDO HAYA ERRORES O INTENTOS DE NAVEGACION A PAGINAS NO EXISTENTES
+//PASO 3 - ARMAR COMPONENTE ERROR QUE SE MUESTRA CUANDO ESTE OCURRA O ANTE INTENTO DE NAVEGACION HACIA CONTENIDO INEXISTENTE
 
 const ErrorComponent = (padre) => {
-   const msjError = `<div class="center red white-text borde-interior z-depth-4">
+   const msjError = `<div class="center red darken-2 white-text borde-interior z-depth-4">
                         <h2>Error 404</h2>
-                        <p>La página solicitada no se encuentra disponible.</p>
-                        <i class="material-icons large">sentiment_very_dissatisfied</i>
+                        <p>La página solicitada no se encuentra disponible o no existe.</p>
+                        <i class="material-icons">sentiment_very_dissatisfied</i>
                      </div>`
          $(padre).html(msjError)
- }
+}
 
-//PASO 4 - VAMOS A TRABAJAR CONTRA LA URL, OBTENIENDO LA RUTA ACTUAL E IDENTIFICANDO SU HASH
-//(USAMOS EL OBJETO LOCATION Y SU PROPIEDAD HASH). SI "" || '/'  ENTONCES parseLocation = '/'
-
+//PASO 4 - Obtener la RUTA generada, identificar el HASH usando location y su propiedad hash
 const parseLocation = () => location.hash.slice(1).toLowerCase() || '/'
 
+// function pL() { //esto es lo mismo que la línea 21, pero con function convencional
+//    if (location.hash.slice(1).toLowerCase() = "") {
+//       return location.hash.slice(1).toLowerCase()
+//    } else {
+//       return "/"
+//    }
+// }
 
-//PASO 5 - BUSCAMOS LA ACCIÓN EN EL ARRAY routes QUE CORRESPONDE A LA RUTA CON FIND 
+//PASO 5 - BUSCAR LA ACCION EN EL ARRAY De acuerdo a la RUTA obtenida mediante Find()
 const findActionByPath = (path, routes) => routes.find(r => r.path == path || undefined)
 
-//PASO 6 - OBTENER RUTA ACTUAL Y LLAMAR AL MÉTODO CORRESPONDIENTE SEGÚN LA ACCIÓN DETECTADA
+//PASO 6 - Obtener la ruta actual y llamar al método correspondiente acorde a la acción detectada
 const router = () => {
-   //OBTENER RUTA ACTUAL
    const path = parseLocation()
    const {action = 'error'} = findActionByPath(path, routes) || {}
-   // LLAMAMOS AL MÈTODO CORRESPONDIENTE PARA LA ACCIÒN ENCONTRADA
+
    switch (action) {
-     case 'agregar':
-       app.agregar('#app')
-       break
-     case 'listar':
-       app.listar('#app')
-       break
-     case 'buscar':
-       app.buscar('#app')
-       break
-     default:
-       ErrorComponent('#app')
-       break
+      case 'agregar':
+         app.agregar('#app')
+         break
+      case 'listar':
+         app.listar('#app')
+         break
+      case 'buscar':
+         app.buscar('#app')
+         break
+      default:
+         ErrorComponent('#app')
+         break
    }
- }
+}
 
-
- //PASO 7 - CARGAR EL RUTEO AUTOMÁTICO CUANDO SE CARGA LA SPA
- $(document).on('load', ()=> {
+$(document).on('load', ()=> {
    router()
- })
+})
 
- //CADA VEZ QUE SE DETECTA UN CAMBIO EN EL HASH (EJEMPLO la URL CAMBIA DE #/pagina1 a #/pagina2) SE LLAMA A LA FUNCION ROUTER
- $(window).on('hashchange', ()=> {
-     router()
- });
+$(window).on('hashchange', ()=> {
+   router()
+})
