b01d33783dfcd98657e2278cffbe0b2f14d721e5
Fernando O <fernando@vidamobile.com.ar>
2021-04-17 09:48:48 -0300

    Update main.js

--- SPA Terminado/js/main.js
+++ SPA Terminado/js/main.js
@@ -1,12 +1,17 @@
+// PASO 1 - VERIFICAMOS EL ARCHIVO PRODUCTOS.JS e INDEX.HTML
+
+
+// PASO 2 - INSTANCIAMOS CONTROLADOR DE PRODUCTOS
 const app = new ProductoController(new ProductoModel(), new ProductoView())
 
-// LISTA DE RUTAS (ASOCIAR A CADA ACCION)
 const routes = [
-                  {path: '', action: 'agregar' },
-                  {path: '/', action: 'agregar' },
-                  {path: '/pagina1', action: 'listar' },
-                  {path: '/pagina2', action: 'buscar' }
-                ]
+  {path: '', action: 'agregar' },
+  {path: '/', action: 'agregar' },
+  {path: '/pagina1', action: 'listar' },
+  {path: '/pagina2', action: 'buscar' }
+]
+
+// PASO 3 - ARMAMOS EL COMPONENTE ERROR PARA MOSTRAR CUANDO HAYA ERRORES O INTENTOS DE NAVEGACION A PAGINAS NO EXISTENTES
 
 const ErrorComponent = (padre) => {
    const msjError = `<div class="center red white-text borde-interior z-depth-4">
@@ -17,12 +22,16 @@ const ErrorComponent = (padre) => {
          $(padre).html(msjError)
  }
 
-//OBTENER LA RUTA ACTUAL (USAMOS EL OBJETO LOCATION Y SU PROPIEDAD HASH). SI "" || '/'  ENTONCES parseLocation = '/'
+//PASO 4 - VAMOS A TRABAJAR CONTRA LA URL, OBTENIENDO LA RUTA ACTUAL E IDENTIFICANDO SU HASH
+//(USAMOS EL OBJETO LOCATION Y SU PROPIEDAD HASH). SI "" || '/'  ENTONCES parseLocation = '/'
+
 const parseLocation = () => location.hash.slice(1).toLowerCase() || '/'
 
-//BUSCAMOS LA ACCIÓN EN EL ARRAY routes QUE CORRESPONDE A LA RUTA CON FIND 
+
+//PASO 5 - BUSCAMOS LA ACCIÓN EN EL ARRAY routes QUE CORRESPONDE A LA RUTA CON FIND 
 const findActionByPath = (path, routes) => routes.find(r => r.path == path || undefined)
 
+//PASO 6 - OBTENER RUTA ACTUAL Y LLAMAR AL MÉTODO CORRESPONDIENTE SEGÚN LA ACCIÓN DETECTADA
 const router = () => {
    //OBTENER RUTA ACTUAL
    const path = parseLocation()
@@ -44,6 +53,8 @@ const router = () => {
    }
  }
 
+
+ //PASO 7 - CARGAR EL RUTEO AUTOMÁTICO CUANDO SE CARGA LA SPA
  $(document).on('load', ()=> {
    router()
  })
